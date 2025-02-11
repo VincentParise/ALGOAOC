3cfb2f92796be0c00d1730cb5d53f5e92002916e
vincentp73000 <vincentp73000@gmail.com>
2025-02-11 14:29:03 +0100

    js

--- index.js
+++ index.js
@@ -4,28 +4,57 @@ const test = readFileSync("test.txt", "utf8");
 const final = readFileSync("final.txt", "utf8");
 
 const part1 = (data) => {
+  // On récupère sous forme de tableau entier:
   const results = data.split(",");
   const resultNumbers = results.map((result) => Number(result));
+
   let days = [];
+
   console.log(`Initial State : ${resultNumbers}`);
 
   // initialise un nouveau tableau
-  function calcTab(data) {
-    const newNumbers = data.map((resultNumbers) => resultNumbers);
-    for (let i = 0; i < newNumbers.length; i++) {
-      newNumbers[i]--;
+  function transformData(datas) {
+    // Test si les éléments de tableaux ne sont pas égal à -1
+    const tousPositif = datas.every((data) => data !== -1);
+    if (!tousPositif) {
+      for (let i = 0; i < datas.length; i++) {
+        if (data[i] === -1) {
+          data[i] = 6;
+        }
+      }
+      days.push(1);
+      console.log(
+        `After ${days.reduce(
+          (acc, number) => acc + number,
+          0
+        )} days : ${beforeDatas}`
+      );
+      transformData(beforeDatas);
+      const index = datas.findIndex((data) => data === -1);
+      if (index !== -1) {
+        const beforeDatas = datas.map((data) => (data === -1 ? 6 : data));
+      }
     }
+
+    // On récupère les datas dans un nouveau tableauvavec une decrementation
+    const currentDatas = datas.map((newData) => {
+      return newData - 1;
+    });
+
+    // On incremente la day et on affiche le tableau
     days.push(1);
     console.log(
       `After ${days.reduce(
         (acc, number) => acc + number,
         0
-      )} days : ${newNumbers}`
+      )} days : ${currentDatas}`
     );
-    calcTab(newNumbers);
-  }
-  calcTab(resultNumbers);
 
+    if (days.reduce((acc, number) => acc + number, 0) < 80) {
+      transformData(currentDatas);
+    }
+  }
+  transformData(resultNumbers);
   return "";
 };
 
