1fd043afaed92b9408b1a0f03ed2982e8efaf3ec
vincentp73000 <vincentp73000@gmail.com>
2025-02-11 15:19:23 +0100

    fin js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const part1 = (data) => {
   // On récupère sous forme de tableau entier:
   const fishs = data.split(",").map(Number);
   console.log(fishs);
-  const DAYS = 80;
+  const DAYS = 40;
 
   for (let day = 0; day < DAYS; day++) {
     const saveLength = fishs.length;
@@ -20,53 +20,24 @@ const part1 = (data) => {
       }
     }
   }
-  return { fishs: fishs.length };
-  // const resultNumbers = results.map((result) => Number(result));
-  // let days = [];
-  // console.log(`Initial State : ${resultNumbers}`);
-  // // initialise un nouveau tableau
-  // function transformData(datas) {
-  //   // Test si les éléments de tableaux ne sont pas égal à -1
-  //   const tousPositif = datas.every((data) => data !== -1);
-  //   if (!tousPositif) {
-  //     for (let i = 0; i < datas.length; i++) {
-  //       if (data[i] === -1) {
-  //         data[i] = 6;
-  //       }
-  //     }
-  //     days.push(1);
-  //     console.log(
-  //       `After ${days.reduce(
-  //         (acc, number) => acc + number,
-  //         0
-  //       )} days : ${beforeDatas}`
-  //     );
-  //     transformData(beforeDatas);
-  //     const index = datas.findIndex((data) => data === -1);
-  //     if (index !== -1) {
-  //       const beforeDatas = datas.map((data) => (data === -1 ? 6 : data));
-  //     }
-  //   }
-  //   // On récupère les datas dans un nouveau tableauvavec une decrementation
-  //   const currentDatas = datas.map((newData) => {
-  //     return newData - 1;
-  //   });
-  //   // On incremente la day et on affiche le tableau
-  //   days.push(1);
-  //   console.log(
-  //     `After ${days.reduce(
-  //       (acc, number) => acc + number,
-  //       0
-  //     )} days : ${currentDatas}`
-  //   );
-  //   if (days.reduce((acc, number) => acc + number, 0) < 80) {
-  //     transformData(currentDatas);
-  //   }
-  // }
-  // transformData(resultNumbers);
+  return { fishs, length: fishs.length };
 };
 
-//console.log({ part1: part1(test) });
+const part2 = (datas) => {
+  // On récupère sous forme de tableau entier:
+  const fish = datas.split(",").reduce((acc, curr) => {
+    acc[curr] = acc[curr] + 1;
+    return acc;
+  }, new Array(9).fill(0));
+  const DAYS = 256;
 
-console.log(part1(final));
-//console.log({ test, final });
+  for (let day = 1; day <= DAYS; day++) {
+    const fish0 = fish.shift();
+    fish.push(fish0);
+    fish[6] += fish0;
+  }
+
+  return fish.reduce((acc, curr) => acc + curr, 0);
+};
+
+console.log(part2(final));
